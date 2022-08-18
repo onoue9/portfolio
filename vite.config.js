9e399b7d953eaa95f9d7aaea89c0727c7858ac44
Filipe Onoue <filipeflom@gmail.com>
2022-08-18 18:37:45 -0300

    fix: :adhesive_bandage: fix the endpoint base repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/onoue9.github.io/portfolio/',
+  base: '/portfolio/',
   plugins: [react()]
 })
