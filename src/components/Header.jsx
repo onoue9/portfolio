2562c61506d0d18e9bd5482f7b128a485ae28f0c
Filipe Onoue <filipeflom@gmail.com>
2022-08-19 15:42:14 -0300

    refactor: :recycle: removing justify-center do tailwind class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => (
   <header className="fixed w-screen">
     <nav className="bg-cyan-500 text-white">
-      <ul className="flex justify-center justify-between h-10 items-center p-5">
+      <ul className="flex justify-between h-10 items-center p-5">
         <li className="p-5"><a href="#intro">Intro</a></li>
         <li className="p-5"><a href="#about">About</a></li>
         <li className="p-5"><a href="#project">Project</a></li>
