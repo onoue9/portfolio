179e655c135c94caeaa37c6b105df4e350787f3a
Filipe Onoue <filipeflom@gmail.com>
2022-09-06 14:16:58 -0300

    fix: :adhesive_bandage: fix text error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => (
   <div id="about" className="flex flex-col justify-center text-center w-auto h-screen bg-cyan-500">
       <span className="text-4xl w-1/2 h-auto self-center">About Me</span>
     <div className="flex flex-col overflow-auto bg-white border border-zinc-400 rounded-lg w-1/2 m-1 p-2 self-center">
-      <p>Hi, my name is Fippe pra Onoue Melo. I have 28 years old. I'm from Duque de Caxias in Rio de Janeiro, Brazil.</p>
+      <p>Hi, my name is Filipe Lira Onoue Melo. I have 28 years old. I'm from Duque de Caxias in Rio de Janeiro, Brazil.</p>
       <p>I'm a lover of everything related to technology, geek world and games. That's why I got interested in development and decided to invest in this area.</p>
       <p>I have always enjoyed helping people, and knowing that I can work doing this has motivated me more and more to dedicate myself and seek knowledge to be able to work with that.</p>
       <p>I'm passionate about series and movies, and some RPG game franchises like Final Fantasy, The Elder Scrolls, The Witcher and others. I also like card games, Yugioh and Magic. I play some sports like basketball and football. I like to be with family and friends, making small talk and sometimes singing karaoke.</p>
