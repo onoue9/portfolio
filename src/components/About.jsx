4bc439dc6b5093115f76c76b6ee79584910189f7
Filipe Onoue <filipeflom@gmail.com>
2023-03-10 18:07:12 -0300

    refactor: :recycle: refact About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,62 @@
-import vivi from '../assets/vivi.gif'
+import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
 
-const About = () => (
-  <div id="about" className="flex flex-col justify-center items-center bg-cyan-500 text-white">
-    <strong className="mt-4 text-xl sm:text-3xl sm:mt-20">Sobre mim</strong>
-    <div className="grid gap-2 my-4 mx-4 text-xl sm:grid-rows-2 sm:grid-flow-col sm:mb-20">
-      <p className="flex justify-center items-center p-5 w-full h-full text-center pulse">Olá, meu nome é Filipe Lira Onoue Melo. Eu tenho 28 anos. Sou de Duque de Caxias no Rio de Janeiro, Brasil.</p>
-      <p className="flex justify-center items-center p-5 w-full h-full text-center pulse">Sou amante de tudo relacionado a tecnologia, mundo geek e games. Por isso me interessei pelo desenvolvimento e decidi investir nessa área.</p>
-      <p className="flex justify-center items-center p-5 w-full h-full text-center pulse">Sempre gostei de ajudar as pessoas, e saber que posso trabalhar fazendo oque gosto (2x), tem me motivado cada vez mais a me dedicar e buscar conhecimento para poder trabalhar com isso.</p>
-      <p className="flex justify-center items-center p-5 w-full h-full text-center pulse">Sou apaixonado por séries e filmes, e algumas franquias de jogos de RPG como Final Fantasy, The Elder Scrolls, The Witcher e outros. Eu pratico alguns esportes como basquete e futebol. Gosto de estar com a família e amigos, bater papo e às vezes cantar no karaokê.</p>
-      <p className="flex justify-center items-center p-5 w-full h-full text-center pulse">No momento terminei o curso de Desenvolvimento Web pela Trybe, e estou a procura de uma oportunidade de emprego.</p>
-      <div className="flex justify-center items-center w-full h-full text-center mb-10">
-        <img src={vivi} alt="Vivi" className="w-2/3 rounded-md" />
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+      </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introdução</p>
+        <h2 className={styles.sectionHeadText}>Visão geral.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Eu sou um desenvolvedor de software Fullstack com experiência em Typescript e Javascript,
+        e especialista em frameworks com React, Node.js e Express. Formado pela Trybe desde Novembro de 2020,
+        estou a procura de uma oportunidade para poder colaborar com uma equipe e desenvolver minhas habilidades.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          )
+        )}
       </div>
-    </div>
-  </div>
-)
+    </>
+  )
+}
 
-export default About;
+export default SectionWrapper(About, 'about')
